becd195abedfdf6e81e5cc923f5e34f02c04885c
IRZAN-PC\hp <irzanraka@gmail.com>
2024-04-05 22:12:19 +0700

    ubah format createdAt

--- src/components/noteItem/NoteItemBody.jsx
+++ src/components/noteItem/NoteItemBody.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { showFormattedDate } from '../../utils/data'
 
 function NoteItemBody({ title, body, createdAt }) {
   return (
     <div className="note-item-body">
       <h4>{title}</h4>
-      <p className="created-date">{createdAt}</p>
+      <p className="created-date">{showFormattedDate(createdAt)}</p>
       <p className="body-text">{body}</p>
     </div>
   );
