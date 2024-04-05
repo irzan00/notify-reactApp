70a4fa71febb4f4e84a71ef3c7004e143b65e708
IRZAN-PC\hp <irzanraka@gmail.com>
2024-04-05 17:50:03 +0700

    Mengubah urutan Catatan yang terbaru di depan

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -6,12 +6,12 @@ function NoteList({ notes, onDelete, onArchive}) {
     <div className="note-list">
       {notes.map((note) => (
         <NoteItem 
-        {...note} 
         key={note.id} 
         id={note.id}
         onDelete={onDelete} 
         onArchive={onArchive}
         isArchive={note.archived}
+        {...note} 
         />
       ))}
     </div>
